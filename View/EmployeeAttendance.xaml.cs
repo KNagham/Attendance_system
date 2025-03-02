83312d798999579847f4dd163ae72bc74209aa4a
KNagham <naghoumkhb@gmail.com>
2025-03-02 05:14:20 +0100

    EmployeeAttedenceStatement

---
+++ View/EmployeeAttendance.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Attendance_system.View
+{
+    /// <summary>
+    /// Interaction logic for EmployeeAttendance.xaml
+    /// </summary>
+    public partial class EmployeeAttendance : Window
+    {
+        public EmployeeAttendance()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDashboard(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
